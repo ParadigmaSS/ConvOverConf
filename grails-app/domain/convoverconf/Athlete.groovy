e7d2ceffb546ed4be0cb2d090ff7f9a87c7fa733
João Paulo Busche <joaopaulo.busche@gmail.com>
2016-08-26 10:19:34 -0300

    Add new variables for classes

--- grails-app/domain/convoverconf/Athlete.groovy
+++ grails-app/domain/convoverconf/Athlete.groovy
@@ -3,11 +3,18 @@ package convoverconf
 class Athlete {
 
 	String name
+	String genre
+	Integer age
+	double weight
 
 	static hasMany = [medals: Medal, modalitys: Modality]
 	static belongsTo = [country: Country, team: Team]
     
     static constraints = {
     	team nullable: true
+    }
+
+    String toString() {
+    	return name
     }
 }
--- grails-app/domain/convoverconf/Team.groovy
+++ grails-app/domain/convoverconf/Team.groovy
@@ -10,4 +10,8 @@ class Team {
    
     static constraints = {
     }
+
+    String toString() {
+    	return name
+    }
 }
